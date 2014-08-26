97b0b32f81f278dbd15eeb5a438bdc71b173b93b
diego <diego@diego-ubuntu>
2014-08-26 12:24:04 -0400

    Ultima prueba

--- application/views/index.php
+++ application/views/index.php
@@ -6,4 +6,5 @@
     <?php echo 'VAMOOO MAESTROOOO USTED PUEDEE ¡¡¡¡' ?>
     <?php echo 'jhajhajhahjajajajajadfjkbdsfjkhbds weon culia la wea' ?>
     <?php echo 'eSTO ES UNA MIERDAAAA  ¡¡¡' ?>
+    <?php echo 'Vamos que resulto esta cuestion'; ?>
 </div>
