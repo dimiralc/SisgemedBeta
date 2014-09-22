24cd35b02dc47d62c3d32cf2576988c28214d09c
diego <diego@diego-ubuntu>
2014-09-22 00:32:24 -0300

    Historia Clinica e Index

--- application/views/profesional/historiaClinica.php
+++ application/views/profesional/historiaClinica.php
@@ -29,7 +29,7 @@
 
 					<i class="fa fa-table"></i>
 
-					<span> <a href="<?= base_url();?>profesional/historiasClinicas">Historias Clinicas Recientes</a></span>
+					<span> <a href="#">Datos Generales del Paciente</a></span>
 
 				</div>
 
@@ -60,161 +60,82 @@
 			</div>
 
 			<div class="box-content no-padding">
-
-				<table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
-
-					<thead>
-
-						<tr>
-
-							<th>#</th>
-
-							<th>Company</th>
-
-							<th>Product</th>
-
-							<th>Homepage</th>
-
-							<th>Product description</th>
-
-						</tr>
-
-					</thead>
-
+                            <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
 					<tbody>
-
 						<tr>
+							<td><b>Rut</b></td>
+							<td>172707475</td>
 
-							<td>1</td>
-
-							<td>Oracle</td>
-
-							<td>OracleDB</td>
-
-							<td>http://oracle.com</td>
-
-							<td>SQL server</td>
-
-						</tr>
-
+					</tr>
 						<tr>
-
-							<td>2</td>
-
-							<td>RedHat</td>
-
-							<td>RedHat Advanced Server</td>
-
-							<td>http://redhat.com</td>
-
-							<td>Operation System</td>
-
+							<td><b>Nombre</b></td>
+							<td>Carolina Estefania</td>
+						
 						</tr>
-
 						<tr>
-
-							<td>3</td>
-
-							<td>Microsoft</td>
-
-							<td>Windows</td>
-
-							<td>http://microsoft.com</td>
-
-							<td>Operation System</td>
-
+							<td><b>Materno</b></td>
+							<td>Gomez</td>
+							
 						</tr>
-
 						<tr>
+							<td><b>Paterno</b></td>
+							<td>Sanchez</td>
 
-							<td>4</td>
-
-							<td>Apple</td>
-
-							<td>MacOSX</td>
-
-							<td>http://apple.com</td>
-
-							<td>Operation System</td>
-
-						</tr>
-
+					</tr>
 						<tr>
-
-							<td>5</td>
-
-							<td>Adobe</td>
-
-							<td>Adobe Photoshop</td>
-
-							<td>http://adobe.com</td>
-
-							<td>Design suite</td>
-
+                                                    <td><b>Direccion</b></td>
+							<td>Maipu, 0542</td>
+						
 						</tr>
-
 						<tr>
-
-							<td>6</td>
-
-							<td>Corel</td>
-
-							<td>Corel Draw!</td>
-
-							<td>http://corel.com</td>
-
-							<td>Design suite</td>
-
+							<td><b>Sexo</b></td>
+							<td>Femenino</td>
+						
 						</tr>
-
 						<tr>
-
-							<td>7</td>
-
-							<td>Google</td>
-
-							<td>Chrome</td>
-
-							<td>http://google.com</td>
-
-							<td>Web-browser</td>
-
+							<td><b>Edad</b></td>
+							<td>25</td>
 						</tr>
-
 						<tr>
+                                                        <td><b>Lugar de Nacimiento</b></td>
+							<td>Anotnio Varas, 666, Proividencia</td>
+                                                </tr>
+                                                <tr>
+                                                        <td><b>Nacionalidad</b></td>
+							<td>Chilena</td>
+                                                </tr>
+                                                <tr>
+                                                        <td><b>Grado Máximo de Estudios</b></td>
+							<td>Universitario Incompleto</td>
+                                                </tr>
+                                                <tr>
+                                                        <td><b>Ocupacion</b></td>
+							<td>Enfermera</td>
+                                                </tr>
+                                                <tr>
+                                                        <td><b>Grupo Sanguíneo</td>
+							<td>RH</td>
+                                                </tr>
+                                                <tr>
+							<td><b>Factor RH</b></td>
+                                                        <td>Positivo</td>
 
-							<td>8</td>
-
-							<td>Mozilla</td>
-
-							<td>Firefox</td>
-
-							<td>http://mozilla.org</td>
-
-							<td>Web-browser</td>
-
-						</tr>
-
+                                                </tr>
 					</tbody>
-
 				</table>
-
+                               
 			</div>
-
 		</div>
-
 	</div>
-    <div class="col-xs-4">
-
+   <div class="col-xs-4">
 		<div class="box">
-
 			<div class="box-header">
 
 				<div class="box-name">
 
 					<i class="fa fa-table"></i>
 
-					<span> <a href="<?= base_url();?>profesional/historiasClinicas">Historias Clinicas Recientes</a></span>
+					<span> <a href="#">Datos Historia Clinica   </a></span>
 
 				</div>
 
@@ -243,30 +164,51 @@
 				<div class="no-move"></div>
 
 			</div>
-
 			<div class="box-content no-padding">
+                            <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
+					<tbody>
+						<tr>
+                                                    <td class="text-capitalize"><b>Fecha Actual</b></td>
+							<td>24/09/2014</td>
 
-                            HOLA MUNDO
+					</tr>
+						<tr>
+							<td><b>Ultima Consulta</b></td>
+							<td>20/09/2014</td>
+						
+						</tr>
+						<tr>
+							<td><b>Numero Historia Clinica</b></td>
+							<td>14025</td>
+							
+						</tr>
+						<tr>
+							<td><b>Estado actual del Paciente</b></td>
+                                                        <td>Estado Reservado</td>
 
+                                                </tr>
+                                                <tr>
+							<td><b>DNI del Paciente</b></td>
+                                                        <td>CS123456</td>
+
+                                                </tr>
+						
+					</tbody>
+				</table>
 			</div>
 
 		</div>
 
 	</div>
-
-</div>
-    <div class="row">
-	<div class="col-xs-12">
-
+     <div class="col-xs-4">
 		<div class="box">
-
 			<div class="box-header">
 
 				<div class="box-name">
 
 					<i class="fa fa-table"></i>
 
-					<span> <a href="<?= base_url();?>profesional/historiasClinicas">Historias Clinicas Recientes</a></span>
+					<span> <a href="#">Fotografia del Paciente</a></span>
 
 				</div>
 
@@ -295,147 +237,17 @@
 				<div class="no-move"></div>
 
 			</div>
-
 			<div class="box-content no-padding">
-
-				<table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
-
-					<thead>
-
-						<tr>
-
-							<th>#</th>
-
-							<th>Company</th>
-
-							<th>Product</th>
-
-							<th>Homepage</th>
-
-							<th>Product description</th>
-
-						</tr>
-
-					</thead>
-
+                            <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
 					<tbody>
-
 						<tr>
+                                                    <td><img src="<?= base_url();?>img/avatar2.png" style="margin-left: 15%;"></td>							
 
-							<td>1</td>
-
-							<td>Oracle</td>
-
-							<td>OracleDB</td>
-
-							<td>http://oracle.com</td>
-
-							<td>SQL server</td>
-
-						</tr>
-
-						<tr>
-
-							<td>2</td>
-
-							<td>RedHat</td>
-
-							<td>RedHat Advanced Server</td>
-
-							<td>http://redhat.com</td>
-
-							<td>Operation System</td>
-
-						</tr>
-
-						<tr>
-
-							<td>3</td>
-
-							<td>Microsoft</td>
-
-							<td>Windows</td>
-
-							<td>http://microsoft.com</td>
-
-							<td>Operation System</td>
-
-						</tr>
-
-						<tr>
-
-							<td>4</td>
-
-							<td>Apple</td>
-
-							<td>MacOSX</td>
-
-							<td>http://apple.com</td>
-
-							<td>Operation System</td>
-
-						</tr>
-
-						<tr>
-
-							<td>5</td>
-
-							<td>Adobe</td>
-
-							<td>Adobe Photoshop</td>
-
-							<td>http://adobe.com</td>
-
-							<td>Design suite</td>
-
-						</tr>
-
-						<tr>
-
-							<td>6</td>
-
-							<td>Corel</td>
-
-							<td>Corel Draw!</td>
-
-							<td>http://corel.com</td>
-
-							<td>Design suite</td>
-
-						</tr>
-
-						<tr>
-
-							<td>7</td>
-
-							<td>Google</td>
-
-							<td>Chrome</td>
-
-							<td>http://google.com</td>
-
-							<td>Web-browser</td>
-
-						</tr>
-
-						<tr>
-
-							<td>8</td>
-
-							<td>Mozilla</td>
-
-							<td>Firefox</td>
-
-							<td>http://mozilla.org</td>
-
-							<td>Web-browser</td>
-
-						</tr>
-
+                                                </tr>
+						
+						
 					</tbody>
-
 				</table>
-
 			</div>
 
 		</div>
@@ -443,5 +255,295 @@
 	</div>
     
 
+</div>
+    <div class="row">
+	<div class="col-xs-6">
+
+		<div class="box">
+
+			<div class="box-header">
+
+				<div class="box-name">
+
+					<i class="fa fa-table"></i>
+
+					<span> <a href="#">Evolucion Grafica del Paciente</a></span>
+
+				</div>
+
+				<div class="box-icons">
+
+					<a class="collapse-link">
+
+						<i class="fa fa-chevron-up"></i>
+
+					</a>
+
+					<a class="expand-link">
+
+						<i class="fa fa-expand"></i>
+
+					</a>
+
+					<a class="close-link">
+
+						<i class="fa fa-times"></i>
+
+					</a>
+
+				</div>
+
+				<div class="no-move"></div>
+
+			</div>
+
+			<div class="box-content no-padding">
+                              <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
+					<tbody>
+						<tr>
+                                                    <td><img src="<?= base_url();?>img/Grafico_Lineas_forex.jpg" height="75%" width="95%"></td>							
+
+                                                </tr>
+						
+						
+					</tbody>
+				</table>
+				
+
+			</div>
+
+		</div>
+
+	</div>
+        
+        <div class="col-xs-6">
+
+		<div class="box">
+
+			<div class="box-header">
+
+				<div class="box-name">
+
+					<i class="fa fa-table"></i>
+
+					<span> <a href="#">Diagnósticos</a></span>
+
+				</div>
+
+				<div class="box-icons">
+
+					<a class="collapse-link">
+
+						<i class="fa fa-chevron-up"></i>
+
+					</a>
+
+					<a class="expand-link">
+
+						<i class="fa fa-expand"></i>
+
+					</a>
+
+					<a class="close-link">
+
+						<i class="fa fa-times"></i>
+
+					</a>
+
+				</div>
+
+				<div class="no-move"></div>
+
+			</div>
+
+			<div class="box-content no-padding">
+                               <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
+					<tbody>
+						<tr>
+                                                    <td class="text-capitalize"><b>Último Diagnóstico CIE-10 </b></td>
+							<td>Insuficiencia cardiaca</td>
+
+					</tr>
+						<tr>
+							<td><b>Ultima Inidicacion</b></td>
+							<td>Reposo Absoluto</td>
+						
+						</tr>
+						<tr>
+							<td><b>Último Medicamento Recetado</b></td>
+							<td>HIDRANTA 30 MEQ SOLUCION ELECTROLITICA</td>
+							
+						</tr>
+						<tr>
+							<td><b>Plan de Trabajo</b></td>
+                                                        <td>Control periódico de Presión</td>
+
+                                                </tr>
+                                                <tr>
+							<td><b>Interconsultas</b></td>
+                                                        <td>Cardiologia</td>
+
+                                                </tr>
+                                                <tr>
+							<td><b>Ultimo Consentimiento</b></td>
+                                                        <td>Biopcia Pericardial</td>
+
+                                                </tr>
+						
+					</tbody>
+				</table>    
+				
+
+			</div>
+
+		</div>
+
+	</div>
+        <div class="col-xs-6">
+
+		<div class="box">
+
+			<div class="box-header">
+
+				<div class="box-name">
+
+					<i class="fa fa-table"></i>
+
+					<span> <a href="#">Evolución del Paciente</a></span>
+
+				</div>
+
+				<div class="box-icons">
+
+					<a class="collapse-link">
+
+						<i class="fa fa-chevron-up"></i>
+
+					</a>
+
+					<a class="expand-link">
+
+						<i class="fa fa-expand"></i>
+
+					</a>
+
+					<a class="close-link">
+
+						<i class="fa fa-times"></i>
+
+					</a>
+
+				</div>
+
+				<div class="no-move"></div>
+
+			</div>
+
+			<div class="box-content no-padding">
+                               <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
+					<tbody>
+						<tr>
+                                                    <td class="text-capitalize"><b>Fecha  </b></td>
+							<td>24/09/2014</td>
+
+					</tr>
+						<tr>
+							<td><b>Sintomas y Signos Relevantes</b></td>
+							<td>Evolucion Positiva en rasgos de presion</td>
+						
+						</tr>
+						<tr>
+							<td><b>Diagnostico Anterior</b></td>
+							<td>Insuficiencia Cardiaca</td>
+							
+						</tr>
+						<tr>
+							<td><b>Tratamiento Recibido</b></td>
+                                                        <td>Receta de medicamento Hidranta</td>
+
+                                                </tr>
+                                                <tr>
+							<td><b>Resultado de Tratamiento</b></td>
+                                                        <td>Evolucion Positiva</td>
+
+                                                </tr>
+						
+					</tbody>
+				</table>    
+				
+
+			</div>
+
+		</div>
+
+	</div>
+        <div class="col-xs-12">
+
+		<div class="box">
+
+			<div class="box-header">
+
+				<div class="box-name">
+
+					<i class="fa fa-table"></i>
+
+					<span> <a href="#">Exportar Datos</a></span>
+
+				</div>
+
+				<div class="box-icons">
+
+					<a class="collapse-link">
+
+						<i class="fa fa-chevron-up"></i>
+
+					</a>
+
+					<a class="expand-link">
+
+						<i class="fa fa-expand"></i>
+
+					</a>
+
+					<a class="close-link">
+
+						<i class="fa fa-times"></i>
+
+					</a>
+
+				</div>
+
+				<div class="no-move"></div>
+
+			</div>
+
+			<div class="box-content no-padding">
+                               <table class="table table-striped table-bordered table-hover table-heading no-border-bottom">
+					<tbody>
+						<tr>
+                                                    <td class="text-capitalize"><b>Exportar a PDF </b></td>
+                                                    <td><input type="button" value="PDF" ></td>
+
+					</tr>
+						<tr>
+							<td><b>Exportar a Imagen</b></td>
+							<td><input type="button" value="Imagen" ></td>
+						
+						</tr>
+						<tr>
+							<td><b>Exportar como XML</b></td>
+							<td><input type="button" value="XML" ></td>
+							
+						</tr>
+						<tr>
+							<td><b>Enviar por Correo electronico/b></td>
+                                                        <td><input type="button" value="e-Mail" ></td>
+                                                    </tr>                                        
+						
+					</tbody>
+				</table>    				
+			</div>
+		</div>
+	</div>
 </div>
 </div>
