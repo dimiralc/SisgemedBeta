5501e9617a9ebe0e2d3f4e09853e27317cf303f9
diego <diego@diego-ubuntu>
2014-10-26 15:16:56 -0300

    cambios menores

--- application/views/componentes/navbar.php
+++ application/views/componentes/navbar.php
@@ -2,7 +2,7 @@
     <div class="container-fluid expanded-panel">
         <div class="row">
             <div id="logo"class="col-xs-12 col-sm-2">
-                <a href="#">SISGEMED</a>
+                <a href="#">SISGEMED &reg; | Beta </a>
                 
             </div>
             <div id="top-panel" class="col-xs-12 col-sm-10">
