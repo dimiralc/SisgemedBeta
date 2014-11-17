66cc619db5e918388c08bd633f3895175df9df88
diego <diego@diego-ubuntu>
2014-11-16 23:26:50 -0300

    16/11/2014

--- application/controllers/administrarMedicamentos.php
+++ application/controllers/administrarMedicamentos.php
@@ -1,6 +1,8 @@
 <?php
+
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+
 class AdministrarMedicamentos extends CI_Controller {
 
     function __construct() {
--- application/controllers/administrarTratamientos.php
+++ application/controllers/administrarTratamientos.php
@@ -1,49 +1,1 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class AdministrarTratamientos extends CI_Controller {
-	function __construct(){
-		parent::__construct();
-                $this->load->helper('form');
-                $this->load->model('administrartratamientos_model');
-	}
-	function index(){
-		$data["titulo"] = 'Tratamientos';
-		$data["url_base"] = $this->config->base_url();
-		$this->load->view('componentes/header.php', $data);
-		$this->load->view('componentes/navbar.php');
-                $this->load->view('componentes/sidebar.php');
-		$this->load->view('profesional/administrarTratamientos.php');		
-		$this->load->view('componentes/modal.php');
-		$this->load->view('componentes/footer.php');
-	}
-        
-        function recibirDatos(){
-		$data = array(
-				'id' => $this->input->post('Id'), 
-				'nombre' => $this->input->post('Nombre'),
-				'sistema' => $this->input->post('Sistema'),				
-				'descripcion' => $this->input->post('Descripcion')
-			);
-                switch( $_POST['btotratamiento'] ) {
-                    case "Agregar":                       
-                        $this->administrartratamientos_model->anadirTratamiento($data);
-                        $this->index();
-                    break;
-                    case "Actualizar":                       
-                        $this->administrartratamientos_model->actualizarTratamiento($data);
-                        $this->index();
-                    break;
-                    case "Eliminar":
-                        $this->administrartratamientos_model->eliminarTratamiento($data);
-                        $this->index();                        
-                    break;
-                    case "Buscar":
-                        $this->administrartratamientos_model->buscarTratamiento($data);
-                        $this->index();                        
-                    break;
-                    case "Cancelar":
-                        $this->index();
-                    }
-	}
-}
-?>
+<?phpif (!defined('BASEPATH'))    exit('No direct script access allowed');class AdministrarTratamientos extends CI_Controller {    function __construct() {        parent::__construct();        $this->load->helper('form');        $this->load->model('administrartratamientos_model');    }    function index() {        $data["titulo"] = 'Tratamientos';        $data["url_base"] = $this->config->base_url();        $this->load->view('componentes/header.php', $data);        $this->load->view('componentes/navbar.php');        $this->load->view('componentes/sidebar.php');        $this->load->view('profesional/administrarTratamientos.php');        $this->load->view('componentes/modal.php');        $this->load->view('componentes/footer.php');    }    function recibirDatos() {        $data = array(            'id' => $this->input->post('Id'),            'nombre' => $this->input->post('Nombre'),            'sistema' => $this->input->post('Sistema'),            'descripcion' => $this->input->post('Descripcion')        );        switch ($_POST['btotratamiento']) {            case "Agregar":                $this->administrartratamientos_model->anadirTratamiento($data);                $this->index();                break;            case "Actualizar":                $this->administrartratamientos_model->actualizarTratamiento($data);                $this->index();                break;            case "Eliminar":                $this->administrartratamientos_model->eliminarTratamiento($data);                $this->index();                break;            case "Buscar":                $this->administrartratamientos_model->buscarTratamiento($data);                $this->index();                break;            case "Cancelar":                $this->index();        }    }}
